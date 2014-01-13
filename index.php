a0db617f7e11ce7f489e2420a5e850c263b89033
Trevor Pierce <trevor@lifeblue.com>
2014-01-13 00:05:44 -0600

    Build system working, ignoring build system directory. Added very basic structure to index.php

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
-	<link rel="stylesheet" href="">
+	
+	<!-- Styles -->
+	<!--[if lt IE 9]><link href="/resources/c/css/global-fixed.css" rel="stylesheet"><![endif]-->
+	<!--[if gt IE 8]><!--><link href="/resources/c/css/global.css" rel="stylesheet"><!--<![endif]-->
+	<!-- End Styles -->
+
 </head>
 <body>
 	
+	<header>
+		<!-- Main Navigation -->
+		<?php include("/viws/partials/mainNavigation.php"); ?>
+		<!-- End Main Navigation -->
+	</header>
+
+	<main role="main">
+		
+		<div class="wrapper">
+			
+			<!-- Main Content Goes Here, probably need some logic to pull in this template or others like into different views -->
+
+		</div>
+
+	</main>
+
 </body>
 </html>
