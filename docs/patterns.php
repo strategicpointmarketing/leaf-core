8f789e937545771be2aef8ff1bf02bf7ac965a3f
Trevor Pierce <trevor@lifeblue.com>
2014-01-24 00:11:20 -0600

    started patterns file, was not able to get normalize worked out yet due to lack of mixins

---
+++ docs/patterns.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<?php include('../views/partials/globalHead.php'); ?>
+
+<body>
+	<div class="gd-row gt-row">
+		<div class="gd-columns gt-columns gd-quarter gt-quarter">
+			Side Navigation Goes Here
+		</div>
+		<div class="gd-columns gt-columns gd-three-quarters gt-three-quarters">
+			
+			<!-- Typographic Scale -->
+			<div>
+				<p class="canon-text">Canon Text</p>
+				<p class="paragon-text">Paragon Text</p>
+				<p class="primer-text">Primer Text</p>
+				<p class="normal-text">Normal Text</p>
+				<p class="petite-text">Petite Text</p>
+				<p class="minion-text">Minion Text</p>
+			</div>
+			<!-- End Typographic Scale -->
+
+			<!-- Embeded Fonts -->
+			<!-- End Fonts -->
+
+			<!-- Body Font -->
+			<p>Following Dumbledore's death, Voldemort continues to gain support and increase his power. When Harry turns seventeen, the protection he has at his aunt and uncle's house will be broken. Before that can happen, at Mad Eye Moody's suggestion, Harry flees to the Burrow with his friends, many of whom use Polyjuice Potion to impersonate him so as to confuse any Death Eaters that may attack.</p>
+			<!-- End Body Font -->
+
+			<!-- Lists -->
+			<div>
+				<h2 class="primary-heading">Lists</h2>
+
+				<!-- Ordered Lists -->
+				<h3 class="secondary-heading primer-text">Ordered List</h3>
+				<ol>
+					<li>Plot introduction</li>
+					<li>Plot summary</li>
+					<li>Epilogue</li>
+				</ol>
+				<!-- End Ordered Lists -->
+
+				<!-- Unordered Lists -->
+				<h3 class="secondary-heading primer-text">Unordered List</h3>
+				<ul>
+					<li>Plot introduction</li>
+					<li>Plot summary</li>
+					<li>Epilogue</li>
+				</ul>		
+				<!-- End Unordered Lists -->
+				
+				<!-- Unstyled Lists -->
+				<h3 class="secondary-heading primer-text">Unstyled List</h3>
+				<ul class="unstyled">
+					<li>Plot introduction</li>
+					<li>Plot summary</li>
+					<li>Epilogue</li>
+				</ul>
+				<!-- End Unstyled Lists -->
+			</div>
+			<!-- End Lists -->
+
+		</div>
+	</div>
+</body>
+</html>
