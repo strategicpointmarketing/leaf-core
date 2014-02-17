84355bb3b8ed7eb600031a817b95d11b4e687122
Trevor Pierce <trevor@lifeblue.com>
2014-02-16 22:48:34 -0600

    updates to patterns and added buttons plus multiple CSS 3 mixins

--- docs/patterns.php
+++ docs/patterns.php
@@ -14,60 +14,107 @@
 				</ul>
 			</div>
 			<div class="gd-columns gt-columns gd-three-quarters gt-three-quarters">
-				<h2 data-scroll="type" class="js-scroll-dest primary-heading paragon-text">Type</h2>
+				<div class="gd-row gt-row">
+					<div class="gd-columns gt-columns gd-third gt-third">
+						<div class="gd-row gt-row gm-row media-grid">
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-1">
+									
+								</span>
+							</div>
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-2">
+									
+								</span>
+							</div>
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-3">
+									
+								</span>
+							</div>
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-4">
+									
+								</span>
+							</div>
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-5">
+									
+								</span>
+							</div>
+							<div class="object gd-columns gt-columns gm-columns gd-third gt-third gm-half">
+								<span class="swatch-6">
+									
+								</span>
+							</div>
+						</div>
+					</div>
+					<div class="gd-columns gt-columns gd-two-thirds gt-two-thirds">
 
-				<!-- Typographic Scale -->
-				<div>
-					<p class="canon-text">Canon Text</p>
-					<p class="paragon-text">Paragon Text</p>
-					<p class="primer-text">Primer Text</p>
-					<p class="normal-text">Normal Text</p>
-					<p class="petite-text">Petite Text</p>
-					<p class="minion-text">Minion Text</p>
+						<h2 data-scroll="type" class="js-scroll-dest primary-heading paragon-text">Type</h2>
+
+						<!-- Typographic Scale -->
+						<div>
+							<p class="canon-text">Canon Text</p>
+							<p class="paragon-text">Paragon Text</p>
+							<p class="primer-text">Primer Text</p>
+							<p class="normal-text">Normal Text</p>
+							<p class="petite-text">Petite Text</p>
+							<p class="minion-text">Minion Text</p>
+						</div>
+						<!-- End Typographic Scale -->
+
+						<!-- Embeded Fonts -->
+						<h3 class="secondary-heading primer-text">Embeded Fonts</h3>
+						<!-- End Fonts -->
+
+						<!-- Body Text -->
+						<h3 class="secondary-heading primer-text">Body Text</h3>
+						<p>Following Dumbledore's death, Voldemort continues to gain support and increase his power. When Harry turns seventeen, the protection he has at his aunt and uncle's house will be broken. Before that can happen, at Mad Eye Moody's suggestion, Harry flees to the Burrow with his friends, many of whom use Polyjuice Potion to impersonate him so as to confuse any Death Eaters that may attack.</p>
+						<!-- End Body Text -->
+
+						<!-- Lists -->
+						<h2 data-scroll="lists" class="js-scroll-dest primary-heading paragon-text">Lists</h2>
+						<div>
+							<!-- Ordered Lists -->
+							<h3 class="secondary-heading primer-text">Ordered List</h3>
+							<ol>
+								<li>Plot introduction</li>
+								<li>Plot summary</li>
+								<li>Epilogue</li>
+							</ol>
+							<!-- End Ordered Lists -->
+
+							<!-- Unordered Lists -->
+							<h3 class="secondary-heading primer-text">Unordered List</h3>
+							<ul>
+								<li>Plot introduction</li>
+								<li>Plot summary</li>
+								<li>Epilogue</li>
+							</ul>		
+							<!-- End Unordered Lists -->
+							
+							<!-- Unstyled Lists -->
+							<h3 class="secondary-heading primer-text">Unstyled List</h3>
+							<ul class="unstyled">
+								<li>Plot introduction</li>
+								<li>Plot summary</li>
+								<li>Epilogue</li>
+							</ul>
+							<!-- End Unstyled Lists -->
+						</div>
+						<!-- End Lists -->
+
+						<!-- Buttons -->
+						<h2 data-scroll="buttons" class="js-scroll-dest primary-heading paragon-text">Buttons</h2>
+						<div>
+							<a href="#" class="button primary large">Primary</a>
+						</div>
+						<!-- End Buttons -->
+
+					</div>
 				</div>
-				<!-- End Typographic Scale -->
-
-				<!-- Embeded Fonts -->
-				<h3 class="secondary-heading primer-text">Embeded Fonts</h3>
-				<!-- End Fonts -->
-
-				<!-- Body Text -->
-				<h3 class="secondary-heading primer-text">Body Text</h3>
-				<p>Following Dumbledore's death, Voldemort continues to gain support and increase his power. When Harry turns seventeen, the protection he has at his aunt and uncle's house will be broken. Before that can happen, at Mad Eye Moody's suggestion, Harry flees to the Burrow with his friends, many of whom use Polyjuice Potion to impersonate him so as to confuse any Death Eaters that may attack.</p>
-				<!-- End Body Text -->
-
-				<!-- Lists -->
-				<h2 data-scroll="lists" class="js-scroll-dest primary-heading paragon-text">Lists</h2>
-				<div>
-					<!-- Ordered Lists -->
-					<h3 class="secondary-heading primer-text">Ordered List</h3>
-					<ol>
-						<li>Plot introduction</li>
-						<li>Plot summary</li>
-						<li>Epilogue</li>
-					</ol>
-					<!-- End Ordered Lists -->
-
-					<!-- Unordered Lists -->
-					<h3 class="secondary-heading primer-text">Unordered List</h3>
-					<ul>
-						<li>Plot introduction</li>
-						<li>Plot summary</li>
-						<li>Epilogue</li>
-					</ul>		
-					<!-- End Unordered Lists -->
-					
-					<!-- Unstyled Lists -->
-					<h3 class="secondary-heading primer-text">Unstyled List</h3>
-					<ul class="unstyled">
-						<li>Plot introduction</li>
-						<li>Plot summary</li>
-						<li>Epilogue</li>
-					</ul>
-					<!-- End Unstyled Lists -->
-				</div>
-				<!-- End Lists -->
-
+				
 			</div>
 		</div>
 	</div>
