c518f7c9a37e062f79c146d765fd4454340feae8
Trevor Pierce <trevor@lifeblue.com>
2014-03-17 22:22:16 -0500

    added watch task to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,13 +57,10 @@ module.exports = function(grunt) {
       files: ['test/**/*.html']
     },
     watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
+      files: ['**/*.less'],
+      tasks: ['less'],
+      options: {
+        spawn: true,
       }
     },
     less: {
@@ -83,7 +80,7 @@ module.exports = function(grunt) {
         cwd: 'c/css/',
         src: ['*.css', '!*.min.css'],
         dest: 'c/css/',
-        ext: '.min.css'
+        ext: '.css'
       }
     }
   });
@@ -92,8 +89,8 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   // grunt.loadNpmTasks('grunt-contrib-qunit');
   // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
   // grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
