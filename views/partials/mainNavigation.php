5089ad4b07e97ff151dffe3e23a7acd2650f9f54
Trevor Pierce <trevor@lifeblue.com>
2014-04-27 02:03:46 -0500

    further testing

--- views/partials/mainNavigation.php
+++ views/partials/mainNavigation.php
@@ -1,10 +1,11 @@
 <nav id="main-nav" class="main-nav" role="navigation">
     <div class="wrapper">
-        <ul class="main-nav--list scroll-nav unstyled">
+        <ul class="main-nav--list scroll-nav">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Products</a></li>
+            <li><a href="#">Blog</a></li>
             <li><a href="#">Contact Us</a></li>
         </ul>
     </div>
